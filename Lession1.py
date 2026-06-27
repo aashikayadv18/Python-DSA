6c21b3b11119d0b1efc5e76b35e116dc0b58d230
Aashika Yadav <yadavaashika71@gmail.com>
2026-06-27 12:52:57 +0530

    Add new file

---
+++ Lession1.py
@@ -0,0 +1,31 @@
+#variables
+
+name = "Aashika"
+age = 19
+x = 19
+
+#refrensh count 2 of (age,x)
+
+print(type((name)))
+print(type((name)))
+print(id((name)))
+print(id((age)))
+print(id((x))) #same id of x and age
+
+#name of variable
+  #characters a/A -z/Z
+  # 0 - 9
+  # _ (underscore)
+#start of the varial name
+  # alphabets
+  # underscore (cant't use even numbers)
+#variable are case senstive
+
+a = 21
+A = 22
+
+print(a)
+print(A)
+
+#avoid using keywords
+#variable should be single word
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+name = "Aashika"
+
+# age = 19
+
+# x = 19
+
+# #refrensh count 2 of (age,x)
+
+# print(type((name)))
+# print(type((name)))
+# print(id((name)))
+# print(id((age)))
+# print(id((x))) #s
