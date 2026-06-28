ae59c3f8a98a828102cbff0d0c3dc831d1180f53
Aashika Yadav <yadavaashika71@gmail.com>
2026-06-28 16:57:45 +0530

    update channge

--- L3.py
+++ L3.py
@@ -1,2 +1,28 @@
-#data typs
+#data typs -
+#numerical data type
 
+x = 5
+print(type(x))
+
+price = 25.26
+print(type(price))
+
+num = 3e6
+print(num)
+
+z = 4 + 5j
+print(z)
+print(type(x))
+
+# inbuilt function
+print(abs(-5))
+
+#power (2to the power3)
+print(pow(2,3))
+
+print(round(4.75))
+print(round(4.5))
+
+num = [3,1,5,7,9,1]
+print(min(num))
+print(max(num))
