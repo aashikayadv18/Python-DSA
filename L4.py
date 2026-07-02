4bc760ddb76adffe80a3686f6296b1cfc3da1591
Aashika Yadav <yadavaashika71@gmail.com>
2026-07-02 19:33:29 +0530

    add file

---
+++ L4.py
@@ -0,0 +1,25 @@
+#list data type
+#collection of element
+
+students =["Aashi","Ariba","Anushka","Aakanksha"]
+print(students)
+print(type(students))
+
+list2 = list(students) #list is constructor here
+print(list2)
+
+list3 = list("Ashika")
+print(list3)
+
+list4 = list((3,4,"Aashi"))
+print(list4)
+
+#Accessing the elemnts of the list
+
+l1 = [1,2,3,4,5,6,7,8,9]
+print(l1[3])
+
+arr =[1,6,7,8,9]
+arr.append(11)
+print(arr)
+arr.insert()
