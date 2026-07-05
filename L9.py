ff3822a1dbd5924da2068a00b831807acb270cf7
Aashika Yadav <yadavaashika71@gmail.com>
2026-07-05 23:33:18 +0530

    add new  file

---
+++ L9.py
@@ -0,0 +1,16 @@
+#Type cashting
+#there are two types of type cashting 1. implicit 2. explicit
+
+#implicit type cashting
+
+x = 5
+y = 6.5
+result = x + y
+print(result)
+
+# Explicit type cashting
+
+y = '10'
+print(type(y))
+int_y = int(y)
+print(type(int_y))
